ed609e3acdfb91a5453f9497d582b5cf0e9e974c
heling <www_heling@sina.com>
2022-01-18 12:31:32 +0800

    稀疏数组

---
+++ DataStructures/src/main/java/com/atguigu/sparsearray/SparseArray.java
@@ -0,0 +1,12 @@
+package com.atguigu.sparsearray;
+
+public class SparseArray {
+    public static void main(String[] args) {
+        //创建一个原始的二维数组11*11
+        //0:表示没有棋子，1表示黑子，2表示篮子
+        int chessArr1[][] = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][4] = 2;
+
+    }
+}
