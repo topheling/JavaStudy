dca81637ecba9bf5c1fc6425e4c0df45683a5be0
heling <www_heling@sina.com>
2022-02-25 11:42:06 +0800

    数据结构--逆波兰表达式-计算器

--- DataStructures/src/main/java/com/atguigu/linkedlist/TestStack.java
+++ DataStructures/src/main/java/com/atguigu/linkedlist/TestStack.java
@@ -1,21 +1,30 @@
 package com.atguigu.linkedlist;
 
+import com.atguigu.stack.ReversePolishMultiCalc;
+
 import java.util.Stack;
 
 //演示栈Stack的基本使用
 public class TestStack {
 
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<String>();
-        //入栈
-        stack.add("jack");
-        stack.add("tom");
-        stack.add("smith");
+//        Stack<String> stack = new Stack<String>();
+//        //入栈
+//        stack.add("jack");
+//        stack.add("tom");
+//        stack.add("smith");
+//
+//        //出栈
+//        while (stack.size()>0){
+//            System.out.println(stack.pop()); //pop就是将栈顶的数据取出
+//        }
 
-        //出栈
-        while (stack.size()>0){
-            System.out.println(stack.pop()); //pop就是将栈顶的数据取出
+        String math = "9+(3-1)*3+10/2";
+        try {
+            Double d = ReversePolishMultiCalc.doCalc(ReversePolishMultiCalc.doMatch(math));
+            System.out.println(d);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 }
