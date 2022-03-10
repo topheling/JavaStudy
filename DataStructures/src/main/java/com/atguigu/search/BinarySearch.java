af1035c02d4d15d8115e8d307a1455fb140c6ee0
heling <www_heling@sina.com>
2022-03-10 17:27:15 +0800

    数据结构--二分查找算法

--- DataStructures/src/main/java/com/atguigu/search/BinarySearch.java
+++ DataStructures/src/main/java/com/atguigu/search/BinarySearch.java
@@ -7,15 +7,15 @@ import java.util.List;
 public class BinarySearch {
 
 	public static void main(String[] args) {
-		//int arr[] = { 1, 8, 10, 89,1000,1000, 1234 };
-		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13,14,15,16,17,18,19,20 };
+		int arr[] = { 1, 8, 10, 89,1000,1000, 1234 };
+//		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13,14,15,16,17,18,19,20 };
 
 
 		//
-//		int resIndex = binarySearch(arr, 0, arr.length - 1, 1000);
+//		int resIndex = binarySearch(arr, 0, arr.length - 1, 3);
 //		System.out.println("resIndex=" + resIndex);
 
-		List<Integer> resIndexList = binarySearch2(arr, 0, arr.length - 1, 1);
+		List<Integer> resIndexList = binarySearch2(arr, 0, arr.length - 1, 1000);
 		System.out.println("resIndexList=" + resIndexList);
 	}
 
